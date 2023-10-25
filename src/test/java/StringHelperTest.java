3d7e583d00beb93545596636dbced821a02a050d
flaviaadiniz <flaviaadiniz@gmail.com>
2023-10-24 21:41:12 -0300

    Add StringHelper main class and StringHelperTest

--- src/test/java/StringHelperTest.java
+++ src/test/java/StringHelperTest.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class StringHelperTest {
+
+    @Test
+    public void test() {
+        fail("Not implemented yet");
+    }
+
 }
