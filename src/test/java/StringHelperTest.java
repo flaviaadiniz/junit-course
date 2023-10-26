f1aaf051282089066e91664cace5c31f0445cc82
flaviaadiniz <flaviaadiniz@gmail.com>
2023-10-26 20:43:20 -0300

    Add 2 first tests to StringHelperTest

--- src/test/java/StringHelperTest.java
+++ src/test/java/StringHelperTest.java
@@ -4,9 +4,17 @@ import static org.junit.Assert.*;
 
 public class StringHelperTest {
 
+    StringHelper helper = new StringHelper();
+
     @Test
-    public void test() {
-        fail("Not implemented yet");
+    public void truncateAInFirst2PositionsTest_AInFirst2Positions() {
+        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+    }
+
+
+    @Test
+    public void truncateAInFirst2PositionsTest_AInFirstPosition() {
+        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
     }
 
 }
