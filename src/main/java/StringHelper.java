40ebf7e8f080d7390180b5673ddbeaeedff3794f
flaviaadiniz <flaviaadiniz@gmail.com>
2023-10-26 20:47:44 -0300

    Minor code refactoring on StringHelper

--- src/main/java/StringHelper.java
+++ src/main/java/StringHelper.java
@@ -2,25 +2,28 @@ public class StringHelper {
 
     // AACD => CD  ACD => CD   CDEF => CDEF   CDAA => CDAA
     public String truncateAInFirst2Positions(String str) {
-        if (str.length() <= 2)
+        if (str.length() <= 2) {
             return str.replaceAll("A", "");
+        }
+
 
         String first2Chars = str.substring(0, 2); // gets the first 2 characters
         String stringMinusFirst2Chars = str.substring(2); // creates a new string starting from the index passed as a parameter
 
-        return first2Chars.replaceAll("A", "")
-                + stringMinusFirst2Chars;
+        return first2Chars.replaceAll("A", "") + stringMinusFirst2Chars;
     }
 
     public boolean areFirstAndLastTwoCharactersTheSame(String str) {
 
-        if (str.length() <= 1)
+        if (str.length() <= 1) {
             return false;
-        if (str.length() == 2)
+        }
+
+        if (str.length() == 2) {
             return true;
+        }
 
         String first2Chars = str.substring(0, 2);
-
         String last2Chars = str.substring(str.length() - 2);
 
         return first2Chars.equals(last2Chars);
