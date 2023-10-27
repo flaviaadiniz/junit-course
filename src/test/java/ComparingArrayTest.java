24e7f2c5bc0f1364fa8691bb00414e43d4efb3b0
flaviaadiniz <flaviaadiniz@gmail.com>
2023-10-27 16:25:24 -0300

    Add perfomance test to ComparingArrayTest

--- src/test/java/ComparingArrayTest.java
+++ src/test/java/ComparingArrayTest.java
@@ -15,7 +15,6 @@ public class ComparingArrayTest {
         Arrays.sort(numbers);
 
         assertArrayEquals(expected, numbers);
-
     }
 
 
@@ -24,5 +23,19 @@ public class ComparingArrayTest {
         int[] numbers = null;
         Arrays.sort(numbers);
     }
+
+
+    @Test(timeout = 1000) // tests performance
+    public void arraySort_Performance() {
+        int[] array = {12, 23, 4};
+
+        for (int i = 1; i <= 1000000; i++) {
+            array[0] = i;
+            Arrays.sort(array);
+        }
+    }
+
+
+
 
 }
