00e11ffadb40583984f61042aa91b15762233a7d
flaviaadiniz <flaviaadiniz@gmail.com>
2023-10-27 13:56:44 -0300

    Add examples of @BeforeClass and @AfterClass

--- src/test/java/QuickBeforeAfterTest.java
+++ src/test/java/QuickBeforeAfterTest.java
@@ -1,13 +1,18 @@
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 public class QuickBeforeAfterTest {
+
+    @BeforeClass // runs only once, before the whole class is run - must be static
+    public static void beforeClass() {
+        System.out.println("Before class");
+    }
+
 
     @Before // runs before each test
     public void setUp() {
         System.out.println("Before");
     }
+
 
     @Test
     public void test() {
@@ -19,9 +24,16 @@ public class QuickBeforeAfterTest {
         System.out.println("Test 2 executed");
     }
 
+
     @After // runs after each test
     public void tearDown() {
         System.out.println("After test");
     }
+
+    @AfterClass // runs onyl once, after the whole class is run - must be static
+    public static void afterClass() {
+        System.out.println("After class");
+    }
+
 
 }
