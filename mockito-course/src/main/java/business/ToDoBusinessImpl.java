870727017fe8c031b5c976f0cc497181d1598ae5
flaviaadiniz <flaviaadiniz@gmail.com>
2023-10-30 17:04:48 -0300

    Add ToDo classes

---
+++ mockito-course/src/main/java/business/ToDoBusinessImpl.java
@@ -0,0 +1,34 @@
+package business;
+
+import data.ToDoService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//ToDoBusinessImpl - SUT (system under test)
+//ToDoService - dependency
+
+public class ToDoBusinessImpl {
+
+    private ToDoService toDoService;
+
+
+    public ToDoBusinessImpl(ToDoService toDoService) {
+        this.toDoService = toDoService;
+    }
+
+
+    public List<String> retrieveToDosRelatedToSpring(String user) {
+        List<String> filterToDos = new ArrayList<String>();
+        List<String> toDos = toDoService.retrieveToDos(user);
+
+        for (String todo : toDos) {
+            if (todo.contains("Spring")) {
+                filterToDos.add(todo);
+            }
+        }
+
+        return filterToDos;
+    }
+
+}
---
+++ mockito-course/src/main/java/data/ToDoService.java
@@ -0,0 +1,9 @@
+package data;
+
+import java.util.List;
+
+public interface ToDoService {
+
+    public List<String> retrieveToDos(String user);
+
+}
