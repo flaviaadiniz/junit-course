643e0aedd68f4770851b487bdf0d7aa335d505b9
flaviaadiniz <flaviaadiniz@gmail.com>
2023-11-02 19:33:50 -0300

    Refactor test class name

--- mockito-course/src/test/java/business/ToDoBusinessImplStubTest.java
+++ mockito-course/src/test/java/business/ToDoBusinessImplStubTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-public class ToDoBusinessImplTest {
+public class ToDoBusinessImplStubTest {
 
     @Test
     public void testRetrieveToDosRelatedToSpring_usingAStub() {
