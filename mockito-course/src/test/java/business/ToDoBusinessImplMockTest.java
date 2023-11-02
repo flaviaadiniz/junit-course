822c45209fec96c3ec1077c39fc91931beee9547
flaviaadiniz <flaviaadiniz@gmail.com>
2023-11-02 19:57:13 -0300

    Add test class using Mockito

---
+++ mockito-course/src/test/java/business/ToDoBusinessImplMockTest.java
@@ -0,0 +1,54 @@
+package business;
+
+import data.ToDoService;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ToDoBusinessImplMockTest {
+
+    /*
+    - Mocking is creating objects that simulate the behavior of real objects. Unlike stubs, mocks can be
+    dinamically created from code at runtime.
+    - Mocking offers more funcionality than stubbing
+    - It's possible to verify method calls and a lot of other things
+     */
+
+
+    @Test
+    public void testRetrieveToDosRelatedToSpring_usingAMock() {
+        ToDoService toDoServiceMock = mock(ToDoService.class);
+
+        List<String> toDos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
+
+        when(toDoServiceMock.retrieveToDos("Dummy")).thenReturn(toDos);
+
+        ToDoBusinessImpl toDoBusinessImpl = new ToDoBusinessImpl(toDoServiceMock);
+
+        List<String> filteredToDos = toDoBusinessImpl.retrieveToDosRelatedToSpring("Dummy");
+
+        assertEquals(2, filteredToDos.size());
+    }
+
+
+    @Test
+    public void testRetrieveToDosRelatedToSpring_withEmptyList() {
+        ToDoService toDoServiceMock = mock(ToDoService.class);
+
+        List<String> toDos = Arrays.asList();
+
+        when(toDoServiceMock.retrieveToDos("Dummy")).thenReturn(toDos);
+
+        ToDoBusinessImpl toDoBusinessImpl = new ToDoBusinessImpl(toDoServiceMock);
+
+        List<String> filteredToDos = toDoBusinessImpl.retrieveToDosRelatedToSpring("Dummy");
+
+        assertEquals(0, filteredToDos.size());
+    }
+
+
+}
--- mockito-course/src/test/java/data/ToDoServiceStub.java
+++ mockito-course/src/test/java/data/ToDoServiceStub.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class ToDoServiceStub implements ToDoService {
 
-
     @Override
     public List<String> retrieveToDos(String user) {
         return Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
