789f205eeaf49e65746c424a2debcad4b0b38832
flaviaadiniz <flaviaadiniz@gmail.com>
2023-11-10 17:34:02 -0300

    Add ListTest to test some Mockito methods with List util class

---
+++ mockito-course/src/test/java/business/ListTest.java
@@ -0,0 +1,42 @@
+package business;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ListTest {
+
+    @Test
+    public void shouldReturnListSize() {
+        List listMock = mock(List.class);
+        when(listMock.size()).thenReturn(2);
+        assertEquals(2, listMock.size());
+    }
+
+
+    @Test
+    public void shouldReturnListSizeMultipleValues() {
+        List listMock = mock(List.class);
+        when(listMock.size()).thenReturn(2).thenReturn(3);
+
+        //the first time it's called, it returns 2. the second time, it returns 3
+        assertEquals(2, listMock.size());
+        assertEquals(3, listMock.size());
+    }
+
+
+    @Test
+    public void shouldReturnAnItemFromAList() {
+        List listMock = mock(List.class);
+        when(listMock.get(0)).thenReturn("in28Minutes");
+
+        assertEquals("in28Minutes", listMock.get(0));
+        assertEquals(null, listMock.get(10));
+    }
+
+
+}
