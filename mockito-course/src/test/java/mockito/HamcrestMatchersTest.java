e003d146b8dd4fc69730f15c69655b5d635b9f85
flaviaadiniz <flaviaadiniz@gmail.com>
2023-11-25 22:45:45 -0300

    Add more assertions to test method

--- mockito-course/src/test/java/mockito/HamcrestMatchersTest.java
+++ mockito-course/src/test/java/mockito/HamcrestMatchersTest.java
@@ -1,6 +1,9 @@
 package mockito;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
@@ -13,9 +16,18 @@ public class HamcrestMatchersTest {
     public void test() {
         List<Integer> scores = Arrays.asList(99, 100, 101, 105);
 
-        //scores have 4 items
+        //assert thst scores has 4 items
         assertEquals(scores.size(), 4);
+
+        //assert that scores contais 99
         assertTrue(scores.contains(99));
+
+        //assert that items in score are under 110
+        assertThat(scores, everyItem(lessThan(110)));
+
+        //assert array size
+        Integer[] marks = {1, 2, 3, 4};
+        assertThat(marks, arrayWithSize(4));
 
     }
 
